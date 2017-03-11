f25fbac865a07e90a8aa38c707b4dd3e0dc37928
Tom Guinther <tguinther@vivox.com>
2017-03-11 11:11:35 -0500

    added WithShortType WithContent => WithContentType

--- gorequestext.go
+++ gorequestext.go
@@ -1,14 +1,26 @@
 package gorequest
 
+import "errors"
+
 // WithHeader is an alias for Set
 func (s *SuperAgent) WithHeader(key, value string) *SuperAgent {
 	s.Set(key, value)
 	return s
 }
 
-// WithContent is a short cut for Set("Content-Type",value)
-func (s *SuperAgent) WithContent(value string) *SuperAgent {
+// WithContentType is a short cut for Set("Content-Type",value)
+func (s *SuperAgent) WithContentType(value string) *SuperAgent {
 	s.Set("Content-Type", value)
+	return s
+}
+
+// WithShortType is a short cut for Type(typeStr string)
+func (s *SuperAgent) WithShortType(shortType string) *SuperAgent {
+	if contentType, ok := Types[shortType]; ok {
+		s.WithContentType(contentType)
+	} else {
+		s.Errors = append(s.Errors, errors.New("WithType func: incorrect type \""+shortType+"\""))
+	}
 	return s
 }
 
